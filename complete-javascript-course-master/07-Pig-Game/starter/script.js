bc3b6dd55fd2bafd670f562a6cdf51e64624ca27
vickythakur0398 <vickythakur0398@gmail.com>
2022-08-14 14:34:04 +0530

    All about this keyword

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,1 +1,46 @@
 'use strict';
+//selecting elemets ids and class
+const score1 = document.querySelector('#score--0');
+const score2 = document.querySelector('#score--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+const player0 = document.querySelector('#name--0');
+const player1 = document.querySelector('#name--1');
+
+//initally score is zero
+score1.textContent = 0;
+score2.textContent = 0;
+//hiding dice initially
+diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+//generating random diceroll
+
+btnRoll.addEventListener('click', () => {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  //since we have rolled the dice we want to show dice image also
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  //checking for rule 1 i.e if dice =1 switch player
+  if (dice != 1) {
+    //add dice to the current score
+    currentScore += dice;
+    document.getElementById(`current--${activePLayer}`).textContent =
+      currentScore;
+  } else {
+    //switch player
+    document.getElementById(`current--${activePLayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    // currentScore = 0;
+  }
+});
+
+current1.textContent = dice;
--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,100 @@
 'use strict';
+/*
+console.log(calAge);
+
+
+let a = 'ram';
+console.log(a);
+function calAge(byear) {
+  const age = 2022 - byear;
+  function printAge() {
+    console.log(`you are ${firstname} born in ${age} ok ${byear}`);
+
+    if (byear >= 1990 && byear <= 2000) {
+      var any = 'hi tyoy hjk s';
+      let st = `you yes u ${firstname} you are ${age} years old and born in ${byear}`;
+      console.log(st);
+
+      function add(a, b) {
+        return a + b;
+      }
+      console.log(add(2, 3));
+    }
+    // console.log(add(2, 3));
+    // console.log(st);
+    console.log(any);
+  }
+
+  printAge();
+  return age;
+}
+
+const firstname = 'vicky';
+calAge(1998);
+
+//variable hoisting
+console.log(me);
+// console.log(job);
+// console.log(role);
+var me = 'vicky';
+let job = 'tcs';const role = 'developer';
+
+console.log(add(2, 3));
+console.log(add);
+
+function add(a, b) {
+  return a + b;
+}
+*/
+
+//using This keyword
+console.log(this);
+// op will be widow object
+
+// this inside nowmal function
+const calAge = function (year) {
+  console.log(2022 - year);
+  console.log(this); // as we know inside function this is undefined in strict mode in
+  //soppy mode this will give window object
+};
+
+// calAge(1998);
+
+// this inside arrow function
+const calAgArow = year => {
+  console.log(2022 - year);
+  console.log(this); //In arrow function there is no this keyword i.e it dont have thier own
+  // this keyword so it uses parent scope this which is global this keyword i.e window object
+};
+
+calAgArow(1998);
+
+// this inside method
+const vicky = {
+  year: 1998,
+  calAgee: function () {
+    console.log(this); // year and calAge function
+    return 2022 - this.year;
+    // here the this keyword is used inside a method so it acceses the object that is calling
+    //the method so if we change the ,ethod or borrow the method it will
+    //access those propeties ex i am creating another sameer
+  },
+};
+
+console.log(vicky.calAgee());
+
+const sameer = {
+  year: 1999,
+};
+
+// here i am borrowing a method
+sameer.calAgee = vicky.calAgee;
+console.log(sameer.calAgee()); // here the same this keyowrd is accesing year = 1999
+//which proves thios keyword always points to the object that is calling the method
+// so here it is pointing to sameer
+
+// if we will copy a function and it will simpley become a normal functionb
+// so again this will be undefined in normal function
+const normalF = vicky.calAgee;
+console.log(normalF);
+normalF(); // undefiend and  error bcs this.year will be undefined.year will cause an error
